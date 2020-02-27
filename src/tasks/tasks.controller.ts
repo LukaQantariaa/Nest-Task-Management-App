f7770f2cf18e3189d6f76b01711ae6cdbb6649c3
Luka Kantaria <lkantaria@bl.ge>
2020-02-27 13:03:02 +0400

    Validating task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,6 +9,7 @@ import { Task, TaskStatus } from './tasks.model';
 //DTO
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -42,7 +43,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id: string,
-        @Body('status') status: TaskStatus
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus
     ) {
         return this.tasksService.updateTaskStatus(id, status)
     }
